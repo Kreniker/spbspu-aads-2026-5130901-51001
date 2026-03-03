3c02c477878fb618fc9a024d26c34663bba2d1ae
Kreniker <kitserov.mike@gmail.com>
2026-03-03 20:13:51 +0300

    read and save names

--- kitserov.mihail/S1/main.cpp
+++ kitserov.mihail/S1/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+
 namespace kitserov
 {
   template< class T > class List;
@@ -5,7 +8,7 @@ namespace kitserov
   class LIter
   {
     friend class List< T >;
-    List<T>* node;
+    List< T >* node;
     LIter(List< T >* n) : node(n) {}
   public:
     LIter() : node(nullptr) {}
@@ -47,9 +50,46 @@ namespace kitserov
   public:
     T data;
     List< T >* next;
+    size_t size;
+
     List(T v, List< T >* n) : data(v), next(n) {}
+    LIter< T > begin() 
+    {
+      return LIter< T >(this);
+    }
+    LIter< T > end()
+    {
+      return LIter< T >(nullptr);
+    }
+    List< T >* add(T v) {
+      List< T >* newNode = new List< T >(v, this);
+      return newNode;
+    }
   };
+
+
 }
 
 int main()
-{}
+{
+  using namespace kitserov;
+  size_t names_cap = 8;
+  size_t names_count = 0;
+  std::string* names = new std::string[names_cap];
+  while (true) {
+    std::string name;
+    if (!(std::cin >> name)) {
+      delete[] names;
+      break;
+    }
+    if (names_count >= names_cap) {
+      names_cap *= 2;
+      std::string* tmp = new std::string[names_cap];
+      for (size_t i = 0; i < names_count; ++i) {
+        tmp[i] = names[i];
+      }
+      delete[] names;
+      names = tmp;
+    }
+  }
+}
