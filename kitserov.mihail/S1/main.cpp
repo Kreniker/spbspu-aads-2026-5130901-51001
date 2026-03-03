82077c728c43ee6dbf16483b2befc930b2b1de80
Kreniker <kitserov.mike@gmail.com>
2026-03-03 20:57:46 +0300

    podik Node in List

--- kitserov.mihail/S1/main.cpp
+++ kitserov.mihail/S1/main.cpp
@@ -47,23 +47,44 @@ namespace kitserov
   template< class T >
   class List
   {
-  public:
-    T data;
-    List< T >* next;
+    struct Node {
+      T data;
+      Node* next;
+      Node(T v, Node* n = nullptr) : data(v), next(n) {}
+    };
+    Node* head;
     size_t size;
-
-    List(T v, List< T >* n) : data(v), next(n) {}
+  public:
+    List() : head(nullptr), size(0) {}
+    ~List()
+    {
+      clear();
+    }
     LIter< T > begin() 
     {
-      return LIter< T >(this);
+      return LIter< T >(head);
     }
     LIter< T > end()
     {
       return LIter< T >(nullptr);
     }
-    List< T >* add(T v) {
+    List< T >* add(T v)
+    {
       List< T >* newNode = new List< T >(v, this);
+      size++;
       return newNode;
+    }
+    List< T >* insert(T v)
+    {
+      List< T >* newNode = next->add(v);
+      next = newNode;
+      return newNode;
+    }
+    T& front()
+    {
+      if (!size)
+        throw std::out_of_range("list is empty");
+      return head->data;
     }
   };
 
@@ -76,12 +97,14 @@ int main()
   size_t names_cap = 8;
   size_t names_count = 0;
   std::string* names = new std::string[names_cap];
+  List< List < int > > list_of_lists;
   while (true) {
     std::string name;
     if (!(std::cin >> name)) {
-      delete[] names;
       break;
     }
+    names[names_count] = name;
+    names_count++;
     if (names_count >= names_cap) {
       names_cap *= 2;
       std::string* tmp = new std::string[names_cap];
@@ -91,5 +114,13 @@ int main()
       delete[] names;
       names = tmp;
     }
+    while (true) {
+      
+    }
   }
+  std::cout << "\n";
+  for (size_t i = 0; i < names_count; ++i) {
+    std::cout << names[i] << " ";
+  }
+  return 0;
 }
