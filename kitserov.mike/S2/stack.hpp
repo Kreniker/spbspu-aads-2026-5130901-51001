9a0a557c3a4c44390ce2400ec70f8c11135c508b
Kreniker <kitserov.mike@gmail.com>
2026-03-19 21:38:53 +0300

    Realise stack methods safetly

--- kitserov.mike/S2/stack.hpp
+++ kitserov.mike/S2/stack.hpp
@@ -5,8 +5,24 @@ template < class T >
 class Stack {
   List< T > data_;
 public:
-  void push(const T& rhs);
-  const T& drop();
+  void push(const T& rhs)
+  {
+    try {
+      data_.add(rhs);
+    } catch (const std::out_of_range& e) {
+      throw std::out_of_range("Failed to push element to stack");
+    }
+  }
+  const T& drop()
+  {
+    try {
+      const T& front = data_.front();
+      data_.pop_front();
+      return front;
+    } catch (const std::out_of_range& e) {
+      throw std::out_of_range("Stack is empty");
+    }
+  }
 };
 
 #endif
