3a7e66c0382ebbb2d8a7fc44f6a24f9b25bd3db2
Kreniker <kitserov.mike@gmail.com>
2026-03-20 14:38:43 +0300

    realise stack and queue methods drop and push

--- kitserov.mihail/S2/queue.hpp
+++ kitserov.mihail/S2/queue.hpp
@@ -1,11 +1,26 @@
 #ifdef QUEUE_HPP
 #define QUEUE_HPP
+#include "common/list.hpp"
 namespace kitserov 
 {
-  template < class T >
-  class Queue {
-
-  };
+    class Queue {
+        List< T > data_;
+      public:
+        void push(T& rhs)
+        {
+          data_.add(rhs);
+        }
+        T drop()
+        {
+            try {
+                T value data_.back();
+                data_.pop_back();
+                return value;
+            } catch (const std::out_of_range& e) {
+                throw std::out_of_range("Queue is empty");
+            }
+        }
+      };
 }
 
 #endif
--- kitserov.mihail/S2/stack.hpp
+++ kitserov.mihail/S2/stack.hpp
@@ -4,8 +4,23 @@
 namespace kitserov {
 
  template < class T >
-  class Stack {
-
+ class Stack {
+    List< T > data_;
+  public:
+    void push(T& rhs)
+    {
+      data_.add(rhs);
+    }
+    T drop()
+    {
+      try {
+        T value = data_.front();
+        data_.pop_front();
+        return value;
+      } catch (const std::out_of_range& e) {
+        throw std::out_of_range("Stack is empty");
+      }
+    }
   };
 }
 
