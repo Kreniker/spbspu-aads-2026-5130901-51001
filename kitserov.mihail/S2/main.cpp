f5ac863a14fd927e4f0d36ccb45d22ba79b7d526
Kreniker <kitserov.mike@gmail.com>
2026-03-20 15:08:10 +0300

    read argument line

--- kitserov.mihail/S2/main.cpp
+++ kitserov.mihail/S2/main.cpp
@@ -1,2 +1,21 @@
-int main()
-{}
+#include <fstream>
+#include <iostream>
+#include "queue.hpp"
+#include "stack.hpp"
+
+int main(int argc, char* argv[])
+{
+    using namespace kitserov;
+    if (argc > 2) {
+        std::cerr << "Too many arguments\n";
+        return 1;
+    }
+    bool haveFilename = argc == 2;
+    if (haveFilename) {
+        std::ifstream file(argv[1]);
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file: " << argv[1] << "\n";
+            return 1;
+        }
+    }
+}
