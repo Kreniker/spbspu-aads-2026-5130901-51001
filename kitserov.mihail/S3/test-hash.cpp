04175794ec8e350be2c23d6b0d350fe0c33bb854
Kreniker <kitserov.mike@gmail.com>
2026-05-27 20:10:06 +0300

    rename tests

--- kitserov.mihail/S3/test-hash.cpp
+++ kitserov.mihail/S3/test-hash.cpp
@@ -1,7 +1,5 @@
-#define BOOST_TEST_MODULE HashTests
-
 #include <utility>
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include "hash.hpp"
 
 using namespace kitserov;
