f75acf8091281a6ad6afa77a555bb848a35e2ef3
Kreniker <kitserov.mike@gmail.com>
2026-05-31 20:04:49 +0300

    identity hash

---
+++ kitserov.mihail/F0/identity-hash.hpp
@@ -0,0 +1,13 @@
+#ifndef IDENTITYHASH_HPP
+#define IDENTITYHASH_HPP
+namespace kitserov
+{
+  struct IdentityHash
+  {
+    size_t operator()(int key) const
+    {
+      return static_cast< size_t >(key);
+    }
+  };
+}
+#endif
