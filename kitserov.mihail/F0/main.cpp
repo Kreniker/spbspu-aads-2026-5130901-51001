6a102a4c2c0a1404647a2cfed913eb08a09f6345
Kreniker <kitserov.mike@gmail.com>
2026-05-31 20:04:33 +0300

    start main without commands

--- kitserov.mihail/F0/main.cpp
+++ kitserov.mihail/F0/main.cpp
@@ -1,2 +1,35 @@
+#include "hash-cucushka.hpp"
+#include "identity-hash.hpp"
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
 int main()
-{}
+{
+  using namespace kitserov;
+  using CommandHandler = void (*)(std::ostream& out, std::istream& in, Hash&);
+  HashCucushka< std::string, CommandHandler,
+    IdentityHash, IdentityHash, std::equal_to< std::string > > cmds(20);
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      auto handler = cmds.find(cmd);
+      if (!handler)
+      {
+        throw std::invalid_argument("unknown command");
+      }
+      (*handler)(std::cout, std::cin, trees);
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  return 0;
+}
