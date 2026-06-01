f612d4f5da69d49852d5099e88cd03c449130c66
Kreniker <kitserov.mike@gmail.com>
2026-06-01 15:25:35 +0300

    help

--- kitserov.mihail/F0/utils-save-load.hpp
+++ kitserov.mihail/F0/utils-save-load.hpp
@@ -13,6 +13,26 @@
 
 namespace kitserov
 {
+  void help(std::ostream& out, std::istream&, ItemTable&,
+    CollectionTable&, InventoryTable&)
+  {
+    out << "Available commands:\n"
+      << "  define-item <id> <name> <width> <height> <value>\n"
+      << "  list-items\n"
+      << "  create-collection <collection_name>\n"
+      << "  add <collection_name> <item_id> <amount>\n"
+      << "  show-collection <collection_name>\n"
+      << "  create-inv <inventory_name> <rows> <cols>\n"
+      << "  show-inv <inventory_name>\n"
+      << "  place <inventory_name> <item_id> <x> <y>\n"
+      << "  place-collection <inventory_name> <collection_name>\n"
+      << "  remove <collection_name> <item_id>  <count>\n"
+      << "  find-space <inv_name> <item_id>\n"
+      << "  organize <inv_name>\n"
+      << "  clear <inv_name>\n"
+      << "  save <file_name>\n"
+      << "  load <file_name>\n";
+  }
   namespace persistence_detail
   {
     inline void writeItemSection(std::ostream& out, const ItemTable& items)
